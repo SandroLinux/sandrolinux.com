2cbbbe95a84c95eeeefd5fb0570e737d55078cb9
Sandro Linux <69315452+SandroLinux@users.noreply.github.com>
2022-11-05 23:10:10 +0000

    Adds link to mastodon

--- pages/contactme.js
+++ pages/contactme.js
@@ -10,7 +10,7 @@ export default function Home() {
       <Header></Header>
       <main className={styles.main}>
       <h2>How to contact me</h2>
-      <p>You can contact me at hello@sandrolinux.com</p>
+      <p>You can contact me at hello@sandrolinux.com. You can also contact me on Twitter and <a rel="me" href="https://masthead.social/@sandrolinux">Mastodon</a></p>
       </main>
       <Footer></Footer>
     </div>
